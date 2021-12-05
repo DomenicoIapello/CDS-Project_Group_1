4fe29deba53aaf1bb98fd30554980ef5134f4c16
MarcelBuehlmann <91598698+MarcelBuehlmann@users.noreply.github.com>
2021-12-05 09:26:31 +0100

    Add files via upload

--- src/erlang_project_skeleton_Playerdue.erl
+++ src/erlang_project_skeleton_Playerdue.erl
@@ -7,37 +7,37 @@ start() ->
 
 ttt() ->
     receive
-        {2,2,2,_} -> 
-            frontend:send(jiffy:encode("Player 2 has won")),
+        {2,2,2,_,_,_,_,_,_} -> 
+            supervisor ! {"Player 2 has won"},
             ttt().
 
         {2,_,_,2,_,_,2,_} ->
-            frontend:send(jiffy:encode("Player 2 has won")),
+            supervisor ! {"Player 2 has won"},
             ttt().
 
         {_,2,_,_,2,_,_,2,_} ->
-            frontend:send(jiffy:encode("Player 2 has won")),
+            supervisor ! {"Player 2 has won"},
             ttt().
 
         {_,_,2,_,_,2,_,_,2} -> 
-            frontend:send(jiffy:encode("Player 2 has won")),
+            supervisor ! {"Player 2 has won"},
             ttt().
 
         {_,_,_,2,2,2,_,_,_} ->
-            frontend:send(jiffy:encode("Player 2 has won")),
+            supervisor ! {"Player 2 has won"},
             ttt().
 
         {_,_,_,_,_,_,2,2,2} ->
-            frontend:send(jiffy:encode("Player 2 has won")),
+            supervisor ! {"Player 2 has won"},
             ttt().
 
         {2,_,_,_,2,_,_,_,2} ->
-            frontend:send(jiffy:encode("Player 2 has won")),
+            supervisor ! {"Player 2 has won"},
             ttt().
 
         {_,_,2,_,2,_,2,_,_} ->
-            frontend:send(jiffy:encode("Player 2 has won")),
+            supervisor ! {"Player 2 has won"},
             ttt()
-        {basecase} ->
-            frontend:send(jiffy:encode("no"))
+        {_,_,_,_,_,_,_,_,_} ->
+            supervisor ! {"No for Player 2"},
     end.
--- src/erlang_project_skeleton_Playeruno.erl
+++ src/erlang_project_skeleton_Playeruno.erl
@@ -3,41 +3,41 @@
 
 start() ->
     register(puno, spawn(?module, ttt(), {}),
-    frontend:send(jiffy("ready")).
+    supervisor ] {"ready"}.
 
 ttt() ->
     receive
         {1,1,1,_} -> 
-            frontend:send(jiffy:encode("Player 1 has won")),
+            supervisor ! {"Player 1 has won"},
             ttt().
 
         {1,_,_,1,_,_,1,_} ->
-            frontend:send(jiffy:encode("Player 1 has won")),
+            supervisor ! {"Player 1 has won"},
             ttt().
 
         {_,1,_,_,1,_,_,1,_} ->
-            frontend:send(jiffy:encode("Player 1 has won")),
+            supervisor ! {"Player 1 has won"},
             ttt().
 
         {_,_,1,_,_,1,_,_,1} -> 
-            frontend:send(jiffy:encode("Player 1 has won")),
+            supervisor ! {"Player 1 has won"},
             ttt().
 
         {_,_,_,1,1,1,_,_,_} ->
-            frontend:send(jiffy:encode("Player 1 has won")),
+            supervisor ! {"Player 1 has won"},
             ttt().
 
         {_,_,_,_,_,_,1,1,1} ->
-            frontend:send(jiffy:encode("Player 1 has won")),
+            supervisor ! {"Player 1 has won"},
             ttt().
 
         {1,_,_,_,1,_,_,_,1} ->
-            frontend:send(jiffy:encode("Player 1 has won")),
+            supervisor ! {"Player 1 has won"},
             ttt().
 
         {_,_,1,_,1,_,1,_,_} ->
-            frontend:send(jiffy:encode("Player 1 has won")),
+            supervisor ! {"Player 1 has won"},
             ttt()
-        {basecase} ->
-            frontend:send(jiffy:encode("No"))
+        {_,_,_,_,_,_,_,_,_} ->
+            supervisor ! {"No for Player 1"},
     end.
