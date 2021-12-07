c7a6208a01c66f0625ca199085546f40b6e2ca18
MarcelBuehlmann <91598698+MarcelBuehlmann@users.noreply.github.com>
2021-12-07 17:03:38 +0100

    Add files via upload

--- src/ttt.js
+++ src/ttt.js
@@ -1,10 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById("reset").addEventListener('click', resetGame);
     document.querySelector('#board').addEventListener('click', markCell);
+    
     var current = 0; //marks the current player 0=player1, 1=player2
     var players = ['1', '2']; //array of players
+    var reset = ['0'];
     var cells = 0; //variable to check if another cell is empty
     const data = [0,0,0,0,0,0,0,0,0]; //data of cells 0=not chosen, 1=chosen by player1, 2=chosen by player2
+    var datainjson = "{uba}";
     
+    function resetGame(c) {
+        data = [0,0,0,0,0,0,0,0,0];
+        document.querySelector('#cell1').innerText = " ";
+        document.querySelector("#cell1").disabled = false;
+        cells = 0;
+        current = 0;
+        document.querySelector('#demo').innerText = data;
+    }
 
     function markCell(c) {
         var cell = c.target; //get the clicked cell
@@ -36,22 +48,48 @@ document.addEventListener('DOMContentLoaded', function(){
         if(cells == 9) {
             document.querySelector('#Player').innerText = 'No Player has won, no more empty cells';
         }
+        let textdata = data.toString();
+        datainjson = JSON.stringify(textdata);
+        //don't know why but the same code doesn't work here but works online: https://reqbin.com/req/javascript/uzf79ewc/javascript-post-request-example
+        //posting on this bin http://ptsv2.com/t/vh7me-1638884442
+        sendData(datainjson);
+        document.querySelector('#backenddata').innerText = datainjson;
+    }
+
+    if(data[0] == 1 && data[4] == 1 && data[8] == 1){
+        document.querySelector('#Player').innerText = p1win();
     }
 
     //put the data array in a JSON format
-    var datainjson = JSON.stringify(data);
+    //var datainjson = JSON.stringify(data);
     
-    //create a new HTTP request which can be fetched by the cowboy REST service
-    //(source:https://stackoverflow.com/questions/36975619/how-to-call-a-rest-web-service-api-from-javascript)
-    function sendData() {
-        var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-             if (this.readyState == 4 && this.status == 200) {
-                 alert(this.responseText);
-             }
-        };
-        xhttp.open("POST", "Localhost:8080", true);
-        xhttp.setRequestHeader("Content-type", "application/json");
-        xhttp.send(datainjson);
+    /*create a new HTTP request which can be fetched by the cowboy REST service
+    (source:https://stackoverflow.com/questions/36975619/how-to-call-a-rest-web-service-api-from-javascript)*/
+    function sendData(x) {
+        var url = "http://ptsv2.com/t/vh7me-1638884442/post";
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", url);
+
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.setRequestHeader("Content-Type", "application/json");
+
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4) {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+        }}
+        xhr.send(x);
     }
+
+    /*function p1win(){
+        return 'You\'re the champion! Congratulations!';
+    }
+
+    function test(){
+        const axios = require('axios');
+        return "salut";
+    }*/
+    
+
 })
